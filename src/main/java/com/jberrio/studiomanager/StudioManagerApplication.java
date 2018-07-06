5c8afe18cd3eae757c90fe563a8d73f5245f9eb9
Jose Berrio <aldarisbm@gmail.com>
2018-07-05 22:03:27 -0500

    initial

---
+++ src/main/java/com/jberrio/studiomanager/StudioManagerApplication.java
@@ -0,0 +1,12 @@
+package com.jberrio.studiomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioManagerApplication.class, args);
+	}
+}
