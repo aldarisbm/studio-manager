2d16697ac543975c10a4f66a1976df04a32a416e
Jose Berrio <aldarisbm@gmail.com>
2018-07-20 11:41:25 -0500

    added a couple of static pages

--- src/main/java/com/jberrio/studiomanager/controllers/index.java
+++ src/main/java/com/jberrio/studiomanager/controllers/index.java
@@ -2,13 +2,19 @@ package com.jberrio.studiomanager.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value ="")
 public class index {
     @RequestMapping(value = "")
-    public String indexPage(){
+    public String indexPage(Model model){
+        model.addAttribute("title","Index");
         return "index";
+    }
+    @RequestMapping(value = "register")
+    public String registration(){
+        return "register";
     }
 }
