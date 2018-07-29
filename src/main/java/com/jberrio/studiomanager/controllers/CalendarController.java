ac5e7d74254931451e49d2cbe52a64f0f7f8ce65
Jose Berrio <aldarisbm@gmail.com>
2018-07-28 21:12:32 -0500

    cleaning up some code starting to work on event form!

--- src/main/java/com/jberrio/studiomanager/controllers/CalendarController.java
+++ src/main/java/com/jberrio/studiomanager/controllers/CalendarController.java
@@ -3,6 +3,9 @@ package com.jberrio.studiomanager.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,5 +17,20 @@ public class CalendarController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("calendar/agenda-views");
         return modelAndView;
+    }
+
+    @RequestMapping(value="schedule")
+    public ModelAndView eventForm(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("calendar/eventform");
+
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value="schedule",method=RequestMethod.POST)
+    @ResponseBody
+    public String datePicker(@RequestParam String startDate,String endDate) {
+            return startDate+" "+endDate;
     }
 }
